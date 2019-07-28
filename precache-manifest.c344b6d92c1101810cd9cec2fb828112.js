c7699ef5760e9f4914ed0b4ebecd84cb105395f8
Aya <vostroknutov.alexander@gmail.com>
2019-07-28 19:39:11 +0400

    Updates

---
+++ precache-manifest.c344b6d92c1101810cd9cec2fb828112.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd22de7191f9e172b2e47a5690c7a9e",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "f008af7380861bf35e04",
+    "url": "/portfolio-website/static/css/main.c0f7f750.chunk.css"
+  },
+  {
+    "revision": "25d37b445c2ae7792ece",
+    "url": "/portfolio-website/static/js/2.5fdfb831.chunk.js"
+  },
+  {
+    "revision": "f008af7380861bf35e04",
+    "url": "/portfolio-website/static/js/main.332a6c9e.chunk.js"
+  },
+  {
+    "revision": "0e1e731819752b5e1982",
+    "url": "/portfolio-website/static/js/runtime~main.7784a2cd.js"
+  },
+  {
+    "revision": "1e070fa9039c4fd1b3d5bb1b56d288fb",
+    "url": "/portfolio-website/static/media/chatapp.1e070fa9.png"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/portfolio-website/static/media/css3.9fe7584b.png"
+  },
+  {
+    "revision": "e72df7e1cb024a6e8fe0848fe2e63386",
+    "url": "/portfolio-website/static/media/express.e72df7e1.png"
+  },
+  {
+    "revision": "9ce68bcab87f651b5a6bd16489896ef7",
+    "url": "/portfolio-website/static/media/github.9ce68bca.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/portfolio-website/static/media/html5.2ea01c0e.png"
+  },
+  {
+    "revision": "5134a670b59724475a4921e37b8b4042",
+    "url": "/portfolio-website/static/media/instagram.5134a670.png"
+  },
+  {
+    "revision": "424757c1557470378ff2a6c1191867c8",
+    "url": "/portfolio-website/static/media/js.424757c1.png"
+  },
+  {
+    "revision": "c81f0054edb169d0869acc93b71d2c6a",
+    "url": "/portfolio-website/static/media/mongodb.c81f0054.png"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/portfolio-website/static/media/nodejs.521444a2.png"
+  },
+  {
+    "revision": "cd7bb6bec60f6799aa174b4e07b92ea4",
+    "url": "/portfolio-website/static/media/react.cd7bb6be.png"
+  },
+  {
+    "revision": "8e766e9562509cb3fa14264580c5c0f9",
+    "url": "/portfolio-website/static/media/sass.8e766e95.svg"
+  }
+]);
